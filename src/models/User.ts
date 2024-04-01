8b5f8cc6fa439bc3044f05070df005495796332a
jisunchung <syi06117@gmail.com>
2024-04-01 16:25:05 +0900

    Add : User type and models file path

---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number
+  email: string
+  password: null
+  nickname: string
+  dateofbirth: string
+  profile_image: string
+  storedCities: string[]
+  createdAt: Date
+  updatedAt: Date
+}
