db3363e733a19c75dcbee1ff7b91ce095ad88f24
jisunchung <syi06117@gmail.com>
2024-04-01 18:44:04 +0900

    feat : emotion test in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const contanerStyles = css`
+  background-color: pink;
+`
+
+const Button = styled.button`
+  width: 300px;
+  heigth: 100px;
+`
+
 function Home() {
-  return <div>hello</div>
+  return (
+    <div css={contanerStyles}>
+      home<Button>버튼</Button>
+    </div>
+  )
 }
 
 export default Home
