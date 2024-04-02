82f525bfe66561bf646d8613faecfedf0fc20063
jisunchung <syi06117@gmail.com>
2024-04-02 17:52:10 +0900

    Style : navbar bg color

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,17 +1,21 @@
+import { css } from '@emotion/react'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
+const navBarStyles = css`
+  background-color: #44ad5e;
+`
+
 function NavBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar css={navBarStyles} expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">adonde admin</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
