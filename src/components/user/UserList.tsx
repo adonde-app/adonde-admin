7d3a592244a2d1b6d9490e9cc25b7bde0d1ee5c8
jisunchung <syi06117@gmail.com>
2024-04-16 16:51:45 +0900

    Feat : loading spinner

--- src/components/user/UserList.tsx
+++ src/components/user/UserList.tsx
@@ -1,18 +1,32 @@
 import Flex from '@shared/Flex'
-// import Text from '@shared/Text'
+import Text from '@shared/Text'
 import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+import { colors } from '@/styles/colorPalette'
 import { useQuery } from '@tanstack/react-query'
 import { fetchUsers } from '@api/getUser'
 import Table from 'react-bootstrap/Table'
+import Spinner from 'react-bootstrap/Spinner'
 
 function UserList() {
   const { isLoading, isError, data } = useQuery({
-    queryKey: ['user'],
+    queryKey: ['users'],
     queryFn: fetchUsers,
   })
   console.log(data)
   if (isLoading) {
-    return <span>Loading...</span>
+    return (
+      <Container>
+        <Flex css={loadingContainerStyles}>
+          <span>
+            <Spinner />
+            <Text typography="t2" css={loadingTextStyles}>
+              loading...
+            </Text>
+          </span>
+        </Flex>
+      </Container>
+    )
   }
   if (isError) {
     return <span>다시 시도해주세요</span>
@@ -47,5 +61,13 @@ function UserList() {
 
 const Container = styled.div`
   padding: 24px;
+`
+
+const loadingContainerStyles = css`
+  padding: 16px;
+  border-radius: 4px;
+`
+const loadingTextStyles = css`
+  margin-left: 30px;
 `
 export default UserList
