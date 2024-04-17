3f8f863f9350d9dd5ff6fe3195b268363b78e006
jisunchung <syi06117@gmail.com>
2024-04-17 17:56:53 +0900

    Feat : user info

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,11 +12,11 @@ function NavBar() {
   return (
     <Navbar css={navBarStyles} expand="lg">
       <Container>
-        <Navbar.Brand href="#home">adonde admin</Navbar.Brand>
+        <Navbar.Brand href="/">adonde admin</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/styles/colorPalette.ts
+++ src/styles/colorPalette.ts
@@ -5,6 +5,9 @@ export const colorPalette = css`
     --red: #f44336;
     --blue: #2196f3;
     --green: #4caf50;
+    --lightGreen: #a0dcb0;
+    --yellow: #ffc42a;
+    --orange: #ff902a;
     --white: #fff;
     --black: #212121;
     --grey: #f0efef;
@@ -16,6 +19,9 @@ export const colors = {
   red: 'var(--red)',
   blue: 'var(--blue)',
   green: 'var(--green)',
+  lightGreen: 'var(--lightGreen)',
+  yellow: 'var(--yellow)',
+  orange: 'var(--orange)',
   white: 'var(--white)',
   black: 'var(--black)',
   grey: 'var(--grey)',
